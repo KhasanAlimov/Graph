c8e706dee041056e9454daecd0cd42c5d2d02e3e
KhasanAlimov <hasanalimov021@gmail.com>
2021-03-07 21:18:47 +0600

    Переключатели

--- js/table-schedule.js
+++ js/table-schedule.js
@@ -1,4 +1,5 @@
 
+
 // URL JSON файла
 const URL = '../server/Company-list.json';
 
@@ -105,32 +106,62 @@ fetch(URL)
                 .attr('height', height);
 
     // Горизонтальная шкала
-    let xScaleDate = data.map((item) => {
-            return new Date(item.date)
-    });
+    let xScaleDate = data.map((item) => new Date(item.date));
 
     let xScale = d3.scaleLinear()
                   .domain([0, xScaleDate.length -1])
-                  .range([10, width - 10]);
+                  .range([40, width - 20]);
 
     let xAxis = d3.axisBottom(d3.scaleTime()
                 .domain([d3.min(xScaleDate), d3.max(xScaleDate)])
-                .range([40, width - 10]));
+                .range([40, width - 20]))
+                .tickFormat(d3.timeFormat("%b %Y"));
+
 
     svg.append('g')
       .style('transform', `translate(0, ${(height - 30)}px)`)
       .call(xAxis);
 
+    
+    let allPrices = [];   // Массив всех цен
+    let allNames = [];   // Массив с именами компаний
 
-    // Вертикальная шкала
-    let allPrices = [];
-
+    // Вывод всех цен в один массив
     data.forEach(item => {
-      item.company.forEach(prices => {
-        allPrices.push(prices.price)
+      item.company.forEach(obj => {
+        allPrices.push(obj.price)
+        allNames.push(obj.name)
       })
     })
+    
+    // Фильтер имён компаний в единственном экземпляре
+    let onlyCompanyName = allNames.filter((item, index) => allNames.indexOf(item) === index)
 
+    let companiesButtonBlock = d3.select('#companies-button-block');    // Кнопки переключатели
+
+    let companyNameIndex = 0;
+
+    // Создаём кнопки переключатели между компаниями
+    companiesButtonBlock.selectAll("span")
+                        .data(onlyCompanyName)
+                        .enter()
+                        .append("span")
+                        .text(item => item)
+                        .attr('id', 0)
+                        .on('click', (el, item) => {
+                          companyNameIndex = onlyCompanyName.indexOf(item)
+                          
+                        })
+
+                        console.log(companyNameIndex)
+
+    // Один массив компаний из трёх
+    let companies = [];   // Массив компаний со свойствами
+    data.forEach(item => {
+      companies.push(item.company[companyNameIndex])
+    })
+    
+    // Вертикальная шкала
     let yScale = d3.scaleLinear()
                 .domain([d3.max(allPrices), 0])
                 .range([20, height - 30]);
@@ -140,5 +171,32 @@ fetch(URL)
     svg.append('g')
       .style('transform', 'translate(40px, 0)')
       .call(yAxis);
+
+
+    // Линии
+    let line = d3.line()
+                .defined(item => !isNaN(item.price))
+                .x((item, index) => xScale(index))
+                .y(item => yScale(item.price))
+
+      // Рисуем линии графика
+      svg.append("path")
+      .datum(companies)
+      .attr("fill", "none")
+      .attr("stroke", "steelblue")
+      .attr("stroke-width", 2)
+      .attr("stroke-linejoin", "round")
+      .attr("stroke-linecap", "round")
+      .attr("d", line);
+
+      // Рисуем точки графика
+      svg.selectAll("circle")
+      .data(companies)
+      .enter()
+      .append("circle")
+      .attr("fill", "#538cb9")
+      .attr('r', 3)
+      .attr('cx', (item, index) => xScale(index))
+      .attr('cy', item => yScale(item.price))
 
 });
